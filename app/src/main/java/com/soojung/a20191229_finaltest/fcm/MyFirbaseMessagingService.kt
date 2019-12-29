b5c8145e0190a87a67663cdd1416fa6c7153a8e5
soojung <blessme333@gmail.com>
2019-12-29 15:09:41 +0900

    FCM 에 프로젝트 등록 / 디바이스 토큰을 받으면 로그에 출력

--- app/src/main/java/com/soojung/a20191229_finaltest/fcm/MyFirbaseMessagingService.kt
+++ app/src/main/java/com/soojung/a20191229_finaltest/fcm/MyFirbaseMessagingService.kt
@@ -3,23 +3,30 @@ package com.soojung.a20191229_finaltest.fcm
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
-class MyFirbaseMessagingService : FirebaseMessagingService() {
+class MyFirebaseMessagingService : FirebaseMessagingService() {
 
-//    푸시알림 받으면 실행되는 함수
+    //    푸시알림을 받으면 실행되는 함수
     override fun onMessageReceived(remoteMessage: RemoteMessage?) {
         super.onMessageReceived(remoteMessage)
 
+//     Notification 타입의 푸시에 사용되는 자료
+//        remoteMessage?.notification
 
+//    데이터메세지에 대한 자료
+//        remoteMessage?.data
+
+    }
 
     override fun onNewToken(token: String?) {
         super.onNewToken(token)
 
-
-
+        Log.d("발급받은토큰", token)
 
     }
+
 
 }
